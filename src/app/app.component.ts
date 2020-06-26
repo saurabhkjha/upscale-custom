ae9d363f2bd2e1900e89d16a5716454cf590640d
Jha <saurabh.b.jha@capgemini.com>
2020-06-26 18:07:16 +0530

    upscale custom component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'upscale-custom';
+export class AppComponent implements OnInit {
+  events: Array<any> = [];
+
+  ngOnInit(): void {
+    window.parent.postMessage({ type: 'initialized' }, '*');
+    window.parent.postMessage(
+      { type: 'sizeChanged', data: { height: 300 } },
+      '*'
+    );
+
+    window.addEventListener('message', (event) => {
+      this.handleEvent(event);
+    });
+  }
+
+  handleEvent(event: any) {
+    console.log('event received: ', event);
+  }
 }
